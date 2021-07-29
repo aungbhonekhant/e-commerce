d985e913388c9df5d64ffaaaa414ece356d35ebc
Aung Bhone Khant <aungbhonekhant.dev@gmail.com>
2021-07-29 20:06:52 +0630

    get url from .env file instead of writing hard code in urlConfig file of admin-app and front-end

--- ecommerce-front-end/src/urlConfig.js
+++ ecommerce-front-end/src/urlConfig.js
@@ -1,6 +1,6 @@
-export const api = 'http://localhost:2000/api'
+export const api = process.env.REACT_APP_API_URL;
 
-export const domain = 'http://localhost:2000'
+export const domain = process.env.REACT_APP_DOMAIN;
 
 export const generatePublicUrl = (fileName) => {
     return `http://localhost:2000/public/${fileName}`;
